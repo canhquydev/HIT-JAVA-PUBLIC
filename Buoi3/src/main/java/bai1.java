de314f87eb4b9ee93d9fee0de4accf921c02ca18
canhquydev <canhquydev@hotmail.com>
2024-08-03 21:07:58 +0700

    btvn buoi 3

--- Buoi3/src/main/java/bai1.java
+++ Buoi3/src/main/java/bai1.java
@@ -19,7 +19,7 @@ public class bai1 {
             a = scanner.nextInt();
             b = scanner.nextInt();
             c = scanner.nextInt();
-            if(a + b <= c || a + c <= b || b + c <= a){
+            if(a + b <= c || a + c <= b || b + c <= a || a <= 0 || b <= 0 || c <= 0){
                 System.out.println(-1);
             }
             else {
