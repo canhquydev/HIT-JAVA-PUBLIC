8871a6b2359c3042fcb23d3a5a8d0c1856ea53e9
canhquydev <canhquydev@hotmail.com>
2024-08-03 21:12:28 +0700

    btvn buoi 3

--- Buoi3/src/main/java/bai2.java
+++ Buoi3/src/main/java/bai2.java
@@ -7,28 +7,23 @@ public class bai2 {
         int length, cnt = 0;
         str = scanner.nextLine();
         length = str.length();
-        if(length % 2 == 0){
-            System.out.println(str.toLowerCase());
-        }
-        else {
-            for(int i = 0; i <= (length / 2); i++){
-                char s1, s2;
-                s1 = str.charAt(i);
-                s2 = str.charAt(length - 1);
-                if(s1 == s2){
-                    cnt++;
-                }
-                else {
-                    System.out.println(str.toLowerCase());
-                    cnt = 0;
-                    break;
-                }
-                length--;
+        for(int i = 0; i <= (length / 2); i++){
+            char s1, s2;
+            s1 = str.charAt(i);
+            s2 = str.charAt(length - 1);
+            if(s1 == s2){
+                cnt++;
+            }
+            else {
+                System.out.println(str.toLowerCase());
+                cnt = 0;
+                break;
+            }
+            length--;
 
-            }
-            if (cnt != 0){
-                System.out.println(str.toUpperCase());
-            }
+        }
+        if (cnt != 0){
+            System.out.println(str.toUpperCase());
         }
     }
 }
